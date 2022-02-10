720bed60f450f19e30a554b7c26861815303b9b9
ronito19 <91139198+ronito19@users.noreply.github.com>
2022-02-10 21:35:17 +0100

    ComprobarFecha

--- models/models.py
+++ models/models.py
@@ -19,7 +19,9 @@
 
 #Comentario
 
-from odoo import models, fields, api
+from odoo import models, fields, api, exceptions
+from datetime import date
+from dateutil.relativedelta import *
 
 class departamento(models.Model):
 	_name ='proyectos.departamento'
@@ -43,6 +45,21 @@ class empleado(models.Model):
 	fechaNacimiento = fields.Date(string='Fecha nacimiento', required=True, default = fields.date.today())
 	direccionEmpleado = fields.Char(string='Direccion', required=True)
 	telefonoEmpleado = fields.Char(string='Telefono')
+	edad = fields.Integer(string='Edad', compute='_getEdad')
+
+	@api.depends('fechaNacimiento')
+	def _getEdad(self):
+		hoy = date.today()
+		for empleado in self:
+			empleado.edad = relativedelta(hoy, empleado.fechaNacimiento).years
+
+	@api.constrains('dniEmpleado')
+	def _checkDNI(self):
+		for empleado in self:
+			if (len(empleado.dniEmpleado) > 9 ):
+				raise exceptions.ValidationError("El DNI no puede ser superior a 9 caracteres")
+			if (len(empleado.dniEmpleado) < 9 ):
+				raise exceptions.ValidationError("El DNI no puede tener menos de 9 caracteres")	
 
 	#Relacion entre tablas
 	departamento_id = fields.Many2one('proyectos.departamento', string='Empleados')
@@ -62,5 +79,14 @@ class proyecto(models.Model):
 	fechaInicio = fields.Date(string='Fecha de inicio', required=True)
 	fechaFin = fields.Date(string='Fecha de fin', required=True)
 
+	@api.depends('fechaInicio')
+	def _checkFechaInicio(self):
+		hoy = date.today()
+		for proyecto in self:
+			proyecto.fechaInicio
+			dias = relativedelta(hoy, proyecto.fechaInicio).days
+			if (dias < 0):
+				raise exceptions.ValidationError("La fecha no puede ser anterior a hoy")
+	
 	#Relacion entre tablas
 	empleado_id = fields.Many2many('proyectos.empleado', string='Empleados')
